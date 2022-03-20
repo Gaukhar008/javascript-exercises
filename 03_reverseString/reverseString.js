3174949cf7e520c131896f082338896346599bcb
Gaukhar <gauhar.abdizhapbarova@gmail.com>
2022-03-20 18:03:26 +0600

    Add function to reverse a word

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reverseWord = '';
+    let letters = word.split('');
+    for (let i = 1; i <= word.length; i++) {
+        reverseWord += letters[letters.length - i]
+    }
+    return reverseWord;
 };
 
 // Do not edit below this line
