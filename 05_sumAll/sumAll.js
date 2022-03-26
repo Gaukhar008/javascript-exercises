0d16052338a4fff0d7905229a324865279d6b526
Gaukhar <gauhar.abdizhapbarova@gmail.com>
2022-03-26 20:54:37 +0600

    Add function sumAll() to pass the tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,19 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, lastNumber) {
+    if (firstNumber > 0 && lastNumber > 0 && typeof firstNumber === 'number' && typeof lastNumber === 'number') {
+    let sum = 0;
+    if (firstNumber < lastNumber) {
+        for(let i = firstNumber; i <= lastNumber; i++) {
+        sum = sum + i;
+        }
+    } else if (firstNumber > lastNumber) {
+        for(let i = firstNumber; i >= lastNumber; i--) {
+            sum = sum + i;
+        }
+    }
+    return sum;
+    } else {
+        return "ERROR";
+    }
 
 };
 
