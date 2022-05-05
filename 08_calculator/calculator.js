34ba902cf1f1323109fb5cc23a330459f9842491
Gauhar Abdizhapbarova <gauhar008@gmail.com>
2022-05-05 22:55:33 +0600

    Solve the exercise and pass all 16 test cases

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(firstNumber, secondNumber) {
+	const total = firstNumber + secondNumber;
+  return total;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNumber, secondNumber) {
+	const total = firstNumber - secondNumber;
+  return total;
 };
 
-const sum = function() {
-	
+const sum = function(arrayOfNumbers) {
+  let total = 0;
+  for(let i = 0; i < arrayOfNumbers.length; i++) {
+    total += arrayOfNumbers[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arrayOfNumbers) {
+  let total = 1;
+  for(let i = 0; i < arrayOfNumbers.length; i++) {
+    total = total * arrayOfNumbers[i]; 
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(firstNumber, secondNumber) {
+	let total = firstNumber ** secondNumber;
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let total = 1;
+  for(let i = 1; i <= number; i++) {
+    if(number === 0) {
+      return 1;
+    }
+    total = total * i;
+  };
+  return total;
 };
 
 // Do not edit below this line
