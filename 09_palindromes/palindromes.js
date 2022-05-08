a5a7c1008829b0f8f42b48d2e4748453b1397363
Gauhar Abdizhapbarova <gauhar008@gmail.com>
2022-05-08 15:16:23 +0600

    Solve the exercise and pass all 6 test cases

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const replaced = string.replace(/[^a-z0-9]/gi, '').toLowerCase();
+    for (let i = 0; i < replaced.length / 2; i++) {
+        if(replaced[i] !== replaced[replaced.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
