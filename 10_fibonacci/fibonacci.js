f684fbf9c75474b819bb4182615b4a6fce08065a
Gauhar Abdizhapbarova <gauhar008@gmail.com>
2022-05-08 16:32:42 +0600

    Solve the exercise and pass all 9 test cases

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if(number < 0) {
+        return 'OOPS';
+    }
+let array = [];
+let first = 0;
+let second = 1;
+array.push(first, second);
+let sum;
 
+for (let i = 0; i < number; i++) {
+sum = first + second;
+array.push(sum);
+first = second;
+second = sum;
+}
+return array[number];
 };
 
 // Do not edit below this line
